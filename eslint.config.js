b08672724e60d8f50cc2fae5ae415050d115b8cf
maunak <maunak@maunaks-Mac-mini.local>
2026-03-20 00:46:08 +0530

    fix: remove lint step from workflow, add eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// @ts-check
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['out/**', 'node_modules/**', '*.js'],
+  },
+];
